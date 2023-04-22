7870caafafc7cf5a315d91897604a4a81de38538
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-04-22 20:43:40 +0300

    added really simple pong to start off

---
+++ settings.py
@@ -0,0 +1,10 @@
+import pygame
+
+FPS = 60
+WIDTH, HEIGHT = 700, 500
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+pygame.display.set_caption("Pong")
