df58cc71d7c98616505ee78c4fcda218de7ccc7a
Oskars <122702900+OskarsTheBest@users.noreply.github.com>
2023-04-23 16:55:38 +0300

    added the ball and collision

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
 import pygame
 
 FPS = 60
+BALL_RADIUS = 7
 WIDTH, HEIGHT = 700, 500
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
